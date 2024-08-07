d4aba29784a0404b22394542fc6f96b3e47133ae
Alvin Alexandro <92777606+alvinalexandro@users.noreply.github.com>
2024-08-07 14:47:52 +1000

    Add files via upload

---
+++ Crazy_Eights/Hello_World_JS.js
@@ -0,0 +1,49 @@
+const fonts = [
+    'Inter_Tiget',
+    'magicalnotes',
+    'Devant_Medium',
+    'Amatic',
+    'Bangers',
+    'Fredericka',
+    'Kirang',
+    'ryeregular',
+    'Indie_F'
+  ];
+  
+  const letters = document.querySelectorAll('.letter');
+  
+  let count=0;
+  
+  const rollIntro = () => {
+    letters.forEach(letter => {
+      let randomFontIndex = Math.floor(Math.random() * fonts.length);
+      let randomFont = fonts[randomFontIndex]; 
+      letter.style.fontFamily=randomFont;
+      }
+    );
+  }
+  
+  let introAnimation = setInterval(function() {
+    rollIntro();
+    if(count>500)
+      clearInterval(introAnimation);
+    count++;
+  },350);
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const myAudio = document.getElementById('myAudio');
+    myAudio.volume = 0.5; // Set volume to 50%
+
+    myAudio.play().catch(error => {
+      console.log("Autoplay prevented. User interaction required.");
+  });
+})
+
+// Example JavaScript to change brightness level dynamically
+function setBrightness(level) {
+  document.documentElement.style.setProperty('--brightness-level', level + '%');
+}
+
+// Example usage: Set brightness to 30%
+setBrightness(100);
